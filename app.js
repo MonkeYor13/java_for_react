42d490b1262deaa1ef151451a914c35370a92e7a
“MonkeYor13” <monkeyor13@gmail.com>
2022-12-03 14:18:31 -0300

    parametros

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("hello");
+// fumcion con parametros
+function hello4(name) {
+	return "hello " + name;
+}
+console.log(hello4("yorvis"));
+console.log(hello4("cesar"));
+console.log(hello4("Jesus"));
+
+function hello5(x, y) {
+	return x + y;
+}
+console.log(hello5(10, 20));
+console.log(hello5(5, 10));
+console.log(hello5(10, 50));
