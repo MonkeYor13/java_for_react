3f827e4e8f1b85a802e78d48cda9fcbb80b1eca1
“MonkeYor13” <monkeyor13@gmail.com>
2022-12-04 18:48:50 -0300

    dom

--- app.js
+++ app.js
@@ -49,26 +49,76 @@
 // }
 // console.log(hello6(10, ));
 
-// OBJETOS EN JSON, estan constituido por una propiedad y un valor o english un key y un value, asi como las funciones podemos colocar un objeto dentro de otro objeto, asi mimo podemos agrgar una array, un propiedad booleana y hasta una funcion dentro del objeto.
+// // OBJETOS EN JSON, estan constituido por una propiedad y un valor o english un key y un value, asi como las funciones podemos colocar un objeto dentro de otro objeto, asi mimo podemos agrgar una array, un propiedad booleana y hasta una funcion dentro del objeto.
+
+// const user = {
+// 	name: "yorvis",
+// 	lastname: "rodriguez",
+// 	age: 38,
+// 	address: {
+// 		country: "argentina",
+// 		city: "buenos aire",
+// 		street: "main street 123",
+// 	},
+// 	friend: ['cesar','pepito'],
+// 	active: true,
+// 	sendMail: function () {
+// 		return "sending mail...";
+// 	},
+// };
+// console.log(user);
+// // para acceder a las propiedades podemos hacerlo ocn la conotacion de punto (.), recuerda que todo lo que no sea una funcion dentro de una objeto son propiedades y las funciones son metodos.
+// console.log(user.age)
+// console.log(user.address.city)
+// console.log(user.friend[1])
+// console.log(user.sendMail())
+
+// // objetos con variables, podemos colocar variables, const dentro de una objeto y llamarlos desde le objeto
+
+// const name = 'laptop'
+// const price = 3000
+
+// const newProduct = {
+// 	name,
+// 	price
+// }
+// console.log(newProduct)
+
+// MONIPULACION DEL DOM, maniupular el documento. document hace referencia al a la pantalla que vemos ne le navegador y con esto podemos acceder a todas la etiquetas o elementos y manipularlos.
+
+// (1)createElement, con esto podemos crear una elemento dentro de HTML y podemos crear con Ctrl+Space a todas la etiquetas disponibles.
+// (2) con append, le decimos que añada en el body un titulo (title que creamos anteriormente en la const title que seria una h1) solo no vermemos nada en la pantalla del navegador porque el h1 esta vacio no tiene texto. tambien podemos agregar una boton.
+// (3) innerText, agregamos un texto al elemento correctamente con JS.
+// (4) addEventListener, esto quiere decir que le podemos agregar eventos o acciones, podemos ver los eventos con Crtl+space
+
+// // (1)
+// const title = document.createElement("h1");
+// const button = document.createElement("button");
+// // (3)
+// title.innerText = 'Hola soy un texto agregado desde JS'
+// button.innerText = 'clic'
+
+// // (4) se lee asi al boton le agregue una escucha con el evento clic, donde ejecuatara una funcion.
+// button.addEventListener("click", function name(params) {
+// 	// console.log('acabo de hacer clic en el boton')
+// 	title.innerText = 'texto fue modificado con JS'
+// 	alert('se hizo un click sobre le boton ahora')
+// })
+
+// // (2)
+// document.body.append(title)
+// document.body.append(button)
+
+// DESTRUCTURANDO, podemos retoanr etiqwuetas HTML desde una funcion o direcmanete al body del documento
 
 const user = {
 	name: "yorvis",
-	lastname: "rodriguez",
 	age: 38,
-	address: {
-		country: "argentina",
-		city: "buenos aire",
-		street: "main street 123",
-	},
-	friend: ['cesar','pepito'],
-	active: true,
-	sendMail: function () {
-		return "sending mail...";
-	},
 };
-console.log(user);
-// para acceder a las propiedades podemos hacerlo ocn la conotacion de punto (.)
-console.log(user.age)
-console.log(user.address.city)
-console.log(user.friend[1])
-console.log(user.sendMail())
+
+function printInfo(user) {
+	return "<h1>Hola " + user.name + "</h1>";
+}
+console.log(printInfo(user));
+document.body.innerHTML = printInfo(user);
+// document.body.innerHTML = "<h1>Hola soy una etiqueta directa</h1>";
